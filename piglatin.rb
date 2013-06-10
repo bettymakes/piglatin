58bdb3591ae772ba19fa5fb86fb99ca30ada010e
blibotics <blibotics@gmail.com>
2013-06-10 15:24:27 -0400

    tested some stuff. not all there yet

--- piglatin.rb
+++ piglatin.rb
@@ -1,31 +1,50 @@
 class PiglatinConverter
 
-def self.convert(word)
+  def self.convert(word)
 
-  string = word.downcase
-    if string == ""
-    	puts ""
+    string = word.downcase
+      if string == ""
+      	puts ""
+        return ""
+        
 
-    elsif string == "nil"
-    	puts "nil"
+      # elsif string == "nil"
+      # 	return nil
 
-    elsif string.index(/[aeiou]/) == 0     #if string does not start with vowel
-    	puts string
+      elsif string.index(/[aeiou]/) == 0     #if string does not start with vowel
+        puts string
+      	return string
 
-    else first_letter = string[0,1]       #Takes first letter of the #{word}
-    string[0]=""     #Removes first letter of the #{word}
-    converted = string<<first_letter<<"ay"     #appends "ay" and first_letter variable to the end of the word.
-	puts converted
 
+      else first_letter = string[0,1]       #Takes first letter of the #{word}
+      string[0]=""     #Removes first letter of the #{word}
+      converted = string<<first_letter<<"ay"     #appends "ay" and first_letter variable to the end of the word.
+  	
+
+
+    return converted
+
+
+    end
   end
-end
+
+  # input: 'fish'
+  # output: 'f'
+
+  # input: 'delete'
+  # output: 'd'
+
+  def find_first_consonant_blob( input_string )
+    # ...
+    # consonant_blob = the result of the code i wrote 
+
+    return consonant_blob
+  end
+
+
 
 end
 
-word = gets.chomp
-PiglatinConverter.convert(word)
-
-
-
-
+# word = gets.chomp
+# PiglatinConverter.convert(word)
 
--- piglatin_test.rb
+++ piglatin_test.rb
@@ -2,6 +2,37 @@ require 'test/unit'
 require_relative 'piglatin'
 
 class TestPiglatin < Test::Unit::TestCase
+	def setup
+		@pig = PiglatinConverter.new
+	end
+
+	def test_PiglatinConverter_can_create_instances_of_itself
+		assert_equal @pig.is_a?(PiglatinConverter), true
+	end
+
+	def test_empty_string_returns_an_empty_string
+		assert_equal PiglatinConverter.convert(""), ""
+	end	
+
+	def test_is_vowel_first_letter_in_word
+		assert_equal PiglatinConverter.convert("apple"), 
+	end
 
 
+	def test_one_is_not_equal_to_zero
+		assert( 1 == 0, 'this should show if the prior thing was false')
+	end
+
+	def test_find_first_consonant_blob
+		@plc = PiglatinConverter.new
+
+		result = @plc.find_first_consonant_blob( 'delete' )
+
+		assert( result == 'd', "I expected find_first... to be d")
+
+		assert_equal( result, 'd', 'custom message goes here if i want')
+	end
 end
+
+# testpiglatin.new
+# testpiglatin.new
